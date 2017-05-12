4821bf1213ebe93e6ee56d2d2a72c406cef3156d
Kálmán Kostenszky <k.kostenszky@gmail.com>
2017-05-12 02:09:12 +0200

    ADD: healthCheck filter

--- docker_persistence/wt_persistence_service/src/main/java/hu/ysmbdt/wt/persistence/FilterConfig.java
+++ docker_persistence/wt_persistence_service/src/main/java/hu/ysmbdt/wt/persistence/FilterConfig.java
@@ -3,6 +3,15 @@
  */
 package hu.ysmbdt.wt.persistence;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -26,7 +35,49 @@ public class FilterConfig {
         registrationBean.setName("authentication");
         AuthFilter authFilter = new AuthFilter(authService);
         registrationBean.setFilter(authFilter);
+        registrationBean.setOrder(2);
+        return registrationBean;
+	}
+	
+	@Bean
+	public FilterRegistrationBean healthCheckFilterRegistrationBean(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setName("healthCheck");
+        registrationBean.setFilter(healthCheckFilter());
+        registrationBean.addUrlPatterns("/aggregate/healthCheck");
         registrationBean.setOrder(1);
         return registrationBean;
+	}
+
+	/**
+	 * @return
+	 */
+	@Bean(name="healthCheck")
+	public Filter healthCheckFilter() {
+		Filter filter = new Filter() {
+		
+			
+			@Override
+			public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+				if (response instanceof HttpServletResponse){
+					((HttpServletResponse)response).setStatus(200);
+				}
+				
+			}
+			
+			@Override
+			public void destroy() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void init(javax.servlet.FilterConfig filterConfig) throws ServletException {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		
+		return filter;
 	}
 }
