36540332b3c8d015a997e303d379d966f6167808
Kálmán Kostenszky <k.kostenszky@gmail.com>
2017-05-13 15:03:08 +0200

    MOD: auth service db connection

--- docker_authentication/app.js
+++ docker_authentication/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var redis = require('redis');
+var crypto = require('crypto');
 
 var PORT = process.env.PORT || 8080;
 
@@ -10,54 +11,88 @@ var app = express();
 app.use(morgan('[:date[iso]] :method :url\t:status'));
 app.use(bodyParser.json({ type: 'application/json' }))
 
-var client = redis.createClient(process.env.DB_PORT, process.env.DB_HOST, {});
-client.set('healthcheck','iam ok');
+var db = redis.createClient(process.env.DB_PORT, process.env.DB_HOST, {});
 
-client.on('connect', function() {
+db.on('connect', function() {
     console.log('Connected to Redis');
 });
+
+db.set('healthCheck','iam ok');
+db.set('Kalman', 'pwd123');
+db.set('Victor','pwd123');
 
 
 var token = 'askljkfds123242'
 
-function getHealth(callback) {
-  return client.get('healthcheck', callback);
+function getFromDB(value,callback) {
+  return db.get(value, callback);
+}
+
+function createToken(userName){
+  var secret = 'a'+new Date().getTime();
+  return crypto.createHmac('sha256',secret).update(userName).digest('hex');
 }
 
 app.get('/auth/healthCheck', function(req,res){
-  getHealth(function(err,reply){
+  getFromDB('healthCheck',function(err,reply){
     var rtrn = (reply == null ? "i am not healthy" : reply);
     res.status(200).send({healthCheck : rtrn });
   })
-      //res.status(200).send('ok');
 });
 
 
 app.post('/auth/login', function (req, res) {
-  if ( req.body.userName === 'Kalman' && req.body.userPassword === 'pwd123') {
-    var response = {
-      userName: req.body.userName,
-      token: token,
-      validTo: 7200
+  getFromDB(req.body.userName, function(err,reply){
+    if (reply === req.body.userPassword){
+      var token = createToken(req.body.userName);
+      var validTo = new Date().getTime() + 600000;
+      db.set('Bearer '+token,validTo);
+      var response = {
+        userName: req.body.userName,
+        token: token,
+        validTo: validTo
+      }
+      res.status(200).send(JSON.stringify(response));
     }
-    res.status(200).send(JSON.stringify(response));
-  } else {
-    var response = {
-      status: 403,
-      message: 'wrong username or password'
+    else {
+      var response = {
+        status: 403,
+        message: 'wrong username or password'
+      }
+      res.status(403).send(JSON.stringify(response));
     }
-    res.status(403).send(JSON.stringify(response));
-  }
+  })
 });
 
 app.post('/auth/check', function (req, res){
-  if (req.body.token === 'Bearer '+token){
-    res.status(204).end();
-  } else {
-    res.status(403).end();
-  }
+  getFromDB(req.body.token, function(err,reply){
+    console.log('r is: '+reply);
+    var validTo = (reply != null ? reply : 0);
+    console.log('v is: '+validTo);
+    console.log('c is: '+new Date().getTime());
+    console.log('validTo > currentTime : '+ (validTo > new Date().getTime()));
+    if (validTo > new Date().getTime()){
+      res.status(204).end();
+    } else {
+      res.status(403).end();
+    }
+  })
 });
 
+app.post('/auth/register', function(req, res){
+  getFromDB(req.body.userName, function(err,reply){
+    if (reply != null){
+      var response = {
+        status : 400,
+        message : 'user already exists'
+      }
+      res.status(400).send(JSON.stringify(response));
+    } else {
+      db.set(req.body.userName, req.body.userPassword);
+      res.status(204).end();
+    }
+  })
+})
 
 // Launch server
 app.listen(PORT);
